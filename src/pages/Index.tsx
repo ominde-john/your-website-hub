b9191501dfa4087425773b1c2328246f605d34d0
John Ominde <ominde@jonzjohn.com>
2026-01-06 11:55:00 +0000

    Add homepage SEO and optimize hero section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet-async";
+
 import Hero from "../components/home/Hero";
 import AboutSection from "../components/home/AboutSection";
 import HighlightsSection from "../components/home/HighlightsSection";
@@ -7,14 +9,25 @@ import JoinCTASection from "../components/home/JoinCTASection";
 
 const HomePage = () => {
   return (
-    <div>
-      <Hero />
-      <AboutSection />
-      <HighlightsSection />
-      <TestimonialsSection />
-      <QuickLinksSection />
-      <JoinCTASection />
-    </div>
+    <>
+      <Helmet>
+        <title>Teksoft Community | Global Technology Community</title>
+        <meta
+          name="description"
+          content="Teksoft Community is a global technology community for developers, innovators, and tech enthusiasts to learn, collaborate, and grow."
+        />
+        <link rel="canonical" href="https://teksoftllc.jonzjohn.com/" />
+      </Helmet>
+
+      <div>
+        <Hero />
+        <AboutSection />
+        <HighlightsSection />
+        <TestimonialsSection />
+        <QuickLinksSection />
+        <JoinCTASection />
+      </div>
+    </>
   );
 };
 
