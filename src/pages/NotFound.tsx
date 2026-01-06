e818151a7e829b1216a1cd8b7220f46247132887
John Ominde <ominde@jonzjohn.com>
2026-01-06 00:01:41 -0800

    Correct 'ERROR!' to 'Error!' in NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404 ERROR!</h1>
+        <h1 className="mb-4 text-4xl font-bold">404 Error!</h1>
         <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found. Looks like the page you're looking for don't exist</p>
         <a href="/" className="text-primary underline hover:text-primary/90">
           Return to Home
