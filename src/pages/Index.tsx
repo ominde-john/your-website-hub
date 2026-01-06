a72cd84c24f7f631fc21a506632bf229cb742441
John Ominde <ominde@jonzjohn.com>
2026-01-06 12:04:16 +0000

    Add SEO metadata to homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import { Helmet } from "react-helmet-async";
-
 import Hero from "../components/home/Hero";
 import AboutSection from "../components/home/AboutSection";
 import HighlightsSection from "../components/home/HighlightsSection";
@@ -10,15 +9,26 @@ import JoinCTASection from "../components/home/JoinCTASection";
 const HomePage = () => {
   return (
     <>
+      {/* SEO METADATA */}
       <Helmet>
-        <title>Teksoft Community | Global Technology Community</title>
+        <title>Teksoft Community | Empowering Technology Everywhere</title>
         <meta
           name="description"
-          content="Teksoft Community is a global technology community for developers, innovators, and tech enthusiasts to learn, collaborate, and grow."
+          content="Teksoft Community is a global technology community for developers, innovators, and tech enthusiasts to learn, collaborate, and grow together."
         />
-        <link rel="canonical" href="https://teksoftllc.jonzjohn.com/" />
+        <meta name="keywords" content="Teksoft, tech community, developers, technology events, innovation" />
+        <meta name="author" content="Teksoft Community" />
+
+        {/* Open Graph (Social Sharing) */}
+        <meta property="og:title" content="Teksoft Community" />
+        <meta
+          property="og:description"
+          content="A global community empowering developers and innovators through collaboration and technology."
+        />
+        <meta property="og:type" content="website" />
       </Helmet>
 
+      {/* PAGE CONTENT */}
       <div>
         <Hero />
         <AboutSection />
